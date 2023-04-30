ddb03bf4c8791dee58b659dcdc6a4e79527d1df2
Anak-caetano <anak.94c@gmail.com>
2023-04-30 07:48:33 -0300

    Interligação das classes do Domínio Bootcamp

--- Desafio 01 - Orientação a Objetos em Java/desafio-pilares-poo-java/src/Main.java
+++ Desafio 01 - Orientação a Objetos em Java/desafio-pilares-poo-java/src/Main.java
@@ -1,4 +1,6 @@
+import desafio.DIO.dominio.Bootcamp;
 import desafio.DIO.dominio.Curso;
+import desafio.DIO.dominio.Dev;
 import desafio.DIO.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -17,15 +19,53 @@ public class Main {
         cursoB.setDescricao("Primeiros passos na linguagem js e integração com react");
         cursoB.setCargaHoraria(95);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria em java");
         mentoria.setDescricao("Tire suas dúvidas em java");
         mentoria.setData(LocalDate.now());
 
 
+        /*
         System.out.println(cursoA);
         System.out.println(cursoB);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Banco Pan Java developer 2023");
+        bootcamp.setDescricao("O programa é uma trilha de 114 horas com cursos, desafios práticos e mentorias em tecnologias essenciais para back-end, de banco de dados e SpringBoot. ");
+        bootcamp.getConteudos().add(cursoA);
+        bootcamp.getConteudos().add(cursoB);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev devAna = new Dev();
+        devAna.setNome("Ana K");
+        devAna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Ana" + devAna.getConteudosInscritos());
+
+        devAna.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos de Ana" + devAna.getConteudosInscritos());
+        System.out.println("Conteudos finalizados de Ana" + devAna.getConteudosFinalizados());
+        System.out.println("XP é :" + devAna.calcularTotalXp());
+
+
+        System.out.println("----------");
+
+
+
+        Dev devJack = new Dev();
+        devJack.setNome("Jacqueline");
+        devJack.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Jack" + devJack.getConteudosInscritos());
+
+        devJack.progredir();
+        devJack.progredir();
+        devJack.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos de Jack" + devJack.getConteudosInscritos());
+        System.out.println("Conteudos finalizados de Jack" + devJack.getConteudosFinalizados());
+        System.out.println("XP é :" + devJack.calcularTotalXp());
     }
 }
---
+++ Desafio 01 - Orientação a Objetos em Java/desafio-pilares-poo-java/src/desafio/DIO/dominio/Dev.java
@@ -0,0 +1,85 @@
+package desafio.DIO.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosFinalizados = new LinkedHashSet<>(); //Só recebe elementos únicos
+
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+
+    }
+
+    public void progredir() {
+
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+
+            this.conteudosFinalizados.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+
+        } else {
+            System.err.println("Você não está matriculado em nenhum de nossos cursos");
+        }
+
+
+    }
+    public double calcularTotalXp(){
+
+       return this.conteudosFinalizados
+               .stream()
+               .mapToDouble(Conteudo::calcularXp)
+               .sum();
+
+
+
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosFinalizados() {
+        return conteudosFinalizados;
+    }
+
+    public void setConteudosFinalizados(Set<Conteudo> conteudosFinalizados) {
+        this.conteudosFinalizados = conteudosFinalizados;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosFinalizados, dev.conteudosFinalizados);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosFinalizados);
+    }
+}
